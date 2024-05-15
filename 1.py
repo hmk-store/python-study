dfa8b4d193d8d0d73f473007cd0b2b837f97a4e8
hmkstore <1481774997@qq.com>
2024-05-15 17:46:29 +0800

    初始化项目版本“

---
+++ 1.py
@@ -0,0 +1,16 @@
+# # 定义函数
+# def fahrenheit_convert(C):
+#     fahrenheit = C * 9/5 + 32
+#     return str(fahrenheit) + '˚F'
+# # 调用函数
+# C2F = fahrenheit_convert(35)
+# print(C2F)
+
+# lyric_length = len('I Cry Out For Magic')
+# print(lyric_length)
+file = open('C://Users/xin/Desktop/工作/text.txt','w')
+file.write('Hello world')
+
+
+
+
